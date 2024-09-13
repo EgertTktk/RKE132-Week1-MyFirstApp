676b4161da1e02c2da0cd3a1f645c104e7e9f563
Egert Roodi <egertroodi@gmail.com>
2024-09-13 09:32:50 +0300

    create new app

---
+++ MyFirsApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:");
+//string-sõne
+string userName;
+userName = Console. ReadLine();
+Console.WriteLine("Hello" + userName);
+//string inerpolation
+
+Console.WriteLine($"Hello, {userName}!");
